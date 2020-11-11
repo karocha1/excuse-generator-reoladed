fa03aa2c1af057b06b125f2972dadde53ca0796f
karocha1 <khernandezarocha@gmail.com>
2020-11-11 20:35:12 +0000

    cambios de primer proyecto java

---
+++ homework.js
@@ -0,0 +1,22 @@
+function OnLoad (failedstudent,who , what , when )
+{
+
+
+
+}
+
+let failedstudent = {
+namestudent: carlos
+grade: segundo
+homeworkdelivered:false
+matter: math
+
+}
+
+let who = ['the dog','my granma','his turtle','my bird'];
+
+let what = ['eat','pissed','crushed','broked'];
+
+let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+
+}
